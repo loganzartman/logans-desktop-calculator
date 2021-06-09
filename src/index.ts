bfce112e4a80ef8614b3de011636851a68272624
nondefault <spam@nondefault.net>
2021-06-09 01:31:22 -0700

    bundle improvements

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import * as base2048 from "base2048";
 import {run, RE} from "./stacker";
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+const filenamePattern = "[name].[contenthash].bundle.js";
 
 module.exports = {
   entry: {
     app: "./src/index.ts"
   },
   output: {
-    filename: "bundle.js"
+    filename: filenamePattern,
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "src/index.html"
-    })
+    }),
+    new MonacoWebpackPlugin({
+      filename: filenamePattern,
+      languages:  [],
+    }),
   ],
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
